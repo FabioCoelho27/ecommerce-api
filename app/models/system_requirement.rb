364afc537bf7937eddd79e5545718baf785ba40f
Fábio Coelho <fabioolivc@gmail.com>
2021-11-24 10:21:46 -0300

    Implementing more tests to system requirement

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -1,3 +1,8 @@
 class SystemRequirement < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false}
+  validates :processor, presence: true
+  validates :memory, presence: true 
+  validates :storage, presence: true
+  validates :video_board, presence: true
+  validates :operational_system, presence: true
 end
