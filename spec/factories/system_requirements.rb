c39afed79d8f73881a6afc61a30232e8beeb7de4
Fábio Coelho <fabioolivc@gmail.com>
2021-11-24 10:02:40 -0300

    Create tests from validate to model System Requirement

--- spec/factories/system_requirements.rb
+++ spec/factories/system_requirements.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :system_requirement do
-    name { "MyString" }
+    sequence(:name) { |n| "Category #{n}" }
     operational_system { "MyString" }
-    storage { "MyString" }
-    processor { "MyString" }
-    memory { "MyString" }
-    video_board { "MyString" }
+    storage { "5GB" }
+    processor { "AMD Ryzen 7" }
+    memory { "2GB" }
+    video_board { "N/A" }
   end
 end
